1f2b3d517e88d69613d945c6c9f5e211c6ef02c1
Fabio-Ottaviani-Dev <F0Dev>
2019-12-15 14:58:37 -0800

    Rev buildspec.yml Conf

--- test.bash
+++ test.bash
@@ -1,14 +1,15 @@
 # Get the external ip for your service
 kubectl get services simple-jwt-api -o wide
+# NAME             TYPE           CLUSTER-IP       EXTERNAL-IP                                                               PORT(S)        AGE   SELECTOR
+# simple-jwt-api   LoadBalancer   10.100.205.118   a6dca99be1f7411ea9afa0ef7fb7c509-1833669863.us-west-2.elb.amazonaws.com   80:30613/TCP   10m   app=simple-jwt-api
+
 
 # Test: Use the external ip url to test the app
-
-
 # 1
-curl -X GET <EXTERNAL-IP>
+curl -X GET 10.100.205.118:8080
 
 # 2 /auth
-export TOKEN=`curl -d '{"email":"wolf@thedoor.com","password":"huff-puff"}' -H "Content-Type: application/json" -X POST <EXTERNAL-IP URL>/auth  | jq -r '.token'`
+export TOKEN=`curl -d '{"email":"wolf@thedoor.com","password":"huff-puff"}' -H "Content-Type: application/json" -X POST 10.100.205.118/auth  | jq -r '.token'`
 
 # 3
 echo $TOKEN
